5c6de09e05e2864f356439f5980fc7a4316e015f
durbin-studo <bracucs@gmail.com>
2016-02-25 04:21:58 +0600

    **Updated with swipe up gesture.. which brings another app drawer consists of toggles and favourite apps. **updated with transitions ** upated with some toggle functions

--- app/src/main/java/com/durbinsoft/amarlauncher/AppDrawerActivity.java
+++ app/src/main/java/com/durbinsoft/amarlauncher/AppDrawerActivity.java
@@ -3,17 +3,25 @@ package com.durbinsoft.amarlauncher;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 
@@ -23,11 +31,11 @@ public class AppDrawerActivity extends Activity {
     CustomApplicationDrawerAdapter customDrawerAdapter;
 
     GridView appDrawerView;
-    LinearLayout appTrayView, slideDrawerView, mainHomeView;
+    LinearLayout appTrayView, slideDrawerView, mainHomeView, bottomDrawerView;
     ImageButton appDrawerHomeButton, appDrawerBuuton1,appDrawerBuuton2,appDrawerBuuton3,appDrawerBuuton4;
-
     private boolean isAppDrawerVisible = false;
     private boolean isAppTrayVisible = true;
+    private boolean isBottomDrawerVisible = false;
 
     DisplayMetrics dmetrics = new DisplayMetrics();
     int widthPixels=dmetrics.widthPixels;
@@ -35,6 +43,10 @@ public class AppDrawerActivity extends Activity {
 
     private boolean appTrayFadeInOut, appTraySlideInOUt, appTrayZoomInOut,appTraySlideLeftRight;
 
+    private boolean wifiState,bluetoothState,rotationState, airplaneState;
+
+    private WifiManager wifiManager;
+    private BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +67,9 @@ public class AppDrawerActivity extends Activity {
         appDrawerBuuton3 = (ImageButton) findViewById(R.id.appDrawerButton3);
         appDrawerBuuton4 = (ImageButton) findViewById(R.id.appDrawerButton4);
 
+
         appDrawerHomeButton = (ImageButton) findViewById(R.id.homeButtonAppDrawer);
+        bottomDrawerView = (LinearLayout) findViewById(R.id.bottomDrawerViewHolder);
         mainHomeView = (LinearLayout) findViewById(R.id.mainHomeView);
         slideDrawerView = (LinearLayout) findViewById(R.id.slideDrawer);
         appTrayView = (LinearLayout) findViewById(R.id.appTrayHolder);
@@ -63,19 +77,27 @@ public class AppDrawerActivity extends Activity {
         appDrawerView.setAdapter(customDrawerAdapter);
 
         appDrawerView.setOnItemClickListener(new AppDrawerClickListener(this, packages));
+
+        //Drawable d = getResources().getDrawable(R.drawable.blueblurbg);
+      //  d.setAlpha(200);
+      //  bottomDrawerView.setBackground(d);
+
+      //  Bitmap tmpImg = BitmapFactory.decodeResource(getResources(),R.drawable.bluetoothblack);
+     //   BitmapDrawable draw = new BitmapDrawable(Bitmap.createScaledBitmap(tmpImg, 16, 16, false));
+     //   airplaneToggleButton.setBackground(draw);
+
         appDrawerView.setVisibility(View.INVISIBLE);
-
         slideDrawerView.setVisibility(View.INVISIBLE);
+        bottomDrawerView.setVisibility(View.INVISIBLE);
 
 
-
-
+        checkSystemStatus();
 
         //below are the code to handle swipe gestures...
 
         mainHomeView.setOnTouchListener(new OnSwipeTouchListener(this) {
             public void onSwipeTop() {
-
+                getBottomDrawerInView();
             }
 
             public void onSwipeRight() {
@@ -89,6 +111,14 @@ public class AppDrawerActivity extends Activity {
             public void onSwipeBottom() {
                 //this should bring down the notification panel
             }
+
+        });
+        bottomDrawerView.setOnTouchListener(new OnSwipeTouchListener(this) {
+
+            public void onSwipeBottom() {
+                getBottomDrawerInView();
+            }
+
 
         });
 
@@ -193,6 +223,9 @@ public class AppDrawerActivity extends Activity {
         else if(isAppTrayVisible){
             getSliderDrawerInView(false);
         }
+         if(isBottomDrawerVisible){
+            getBottomDrawerInView();
+        }
     }
 
     //this will open or close the app drawer.
@@ -210,17 +243,38 @@ public class AppDrawerActivity extends Activity {
         if(direction){
             mainHomeView.animate().translationX(slideDrawerView.getWidth());
             slideDrawerView.setVisibility(View.VISIBLE);
-            slideDrawerView.setAlpha(0.5f);
+            slideDrawerView.setAlpha(0.0f);
             slideDrawerView.animate().translationX(widthPixels).alpha(1.0f);
         }else{
             mainHomeView.animate().translationX(0);
-            slideDrawerView.animate().translationX(-slideDrawerView.getWidth()).setListener(new AnimatorListenerAdapter() {
+            slideDrawerView.animate().translationX(-slideDrawerView.getWidth()).alpha(0.0f).setListener(new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationEnd(Animator animation) {
                     super.onAnimationEnd(animation);
                     slideDrawerView.setVisibility(View.VISIBLE);
                 }
             });
+        }
+    }
+
+    private void getBottomDrawerInView(){
+        // direction true == in and false == out
+        if(isBottomDrawerVisible == false){
+          //  mainHomeView.animate().translationX(slideDrawerView.getWidth());
+            bottomDrawerView.setVisibility(View.VISIBLE);
+            bottomDrawerView.setAlpha(0.0f);
+            bottomDrawerView.animate().translationY(heightPixels).alpha(1.0f);
+            isBottomDrawerVisible = true;
+        }else{
+            //mainHomeView.animate().translationX(0);
+            bottomDrawerView.animate().translationY(bottomDrawerView.getHeight()).alpha(0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    bottomDrawerView.setVisibility(View.VISIBLE);
+                }
+            });
+            isBottomDrawerVisible = false;
         }
     }
 
@@ -269,6 +323,70 @@ public class AppDrawerActivity extends Activity {
         }
     }
 
+    private void changeBrightness(int bVal){
+        switch (bVal){
+            case 121:
+                //auto mode
+                break;
+            default:
+                //any value 0-10 but check, if value is under 10, then always keep it 10
+                if(bVal<=10){
+                    //always brightness val to 10
+                }
+        }
+    }
+
+    private void changeWifiState(boolean val){
+        wifiManager.setWifiEnabled(val);
+        //update ui gfx here for the bottom drawer toggle. create another method and call that to update gfx.
+    }
+
+    private void changeBluetoothState(boolean val){
+        if(val){
+            bluetoothAdapter.enable();
+        }else{
+            bluetoothAdapter.disable();
+        }
+    }
+
+    private void changeRotationState(boolean val){
+        Settings.System.putInt( getContentResolver(), Settings.System.ACCELEROMETER_ROTATION, val ? 1 : 0);
+    }
+
+    private void changeAireplaneState(boolean val){
+
+    }
+
+    private void checkSystemStatus(){
+        //check wifi state,bluetooth state, rotation state, brightness state and set the value accordingly to the view.
+        //call the updateGfx method to update gfx AND BROADCAST LISTERNER FOR ALL THESE SERVICE,, SO THAT ANY CHANGE CAN EFFECT IMMEDIATELY.
+        wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+        wifiState = wifiManager.isWifiEnabled();
+
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothState = bluetoothAdapter.isEnabled();
+
+        try {
+            int result = Settings.System.getInt(getContentResolver(), Settings.System.ACCELEROMETER_ROTATION);
+            if(result==1){
+                rotationState = true;
+            }else{
+                rotationState = false;
+            }
+        } catch (Settings.SettingNotFoundException e) {
+
+        }
+
+
+
+    }
+
+    public void preferenceButtonClicked(View v){
+        //method is invoked when called from the app_drawer.xml layout
+        Intent prefs = new Intent(this, PreferenceScreen.class);
+        startActivityForResult(prefs,0);
+    }
 
     public class PackageChangeBroadCastListener extends BroadcastReceiver{
 
